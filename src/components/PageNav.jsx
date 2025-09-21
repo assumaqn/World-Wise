01293aaaa3b4dca1c7238a47f201f6302d58c828
unknown <somegetiye@gmail.com>
2025-09-21 02:58:05 -0700

    update on building pages

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,22 @@
 import { NavLink } from "react-router-dom";
 import style from "./PageNav.module.css";
+import Logo from "./Logo.jsx";
 
 function PageNav() {
   return (
     <nav className={style.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/Pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Login" className={style.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
